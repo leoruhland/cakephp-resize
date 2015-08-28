b8d4a89450c96b8d86f28eec21689830a472ce41
Leonardo Ruhland <leoruhland@gmail.com>
2015-08-28 07:38:51 -0300

    Initial commit

---
+++ src/Controller/ResizeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Resize\Controller;
+
+use Resize\Controller\AppController;
+use Gregwar\Image\Image;
+
+class ResizeController extends AppController
+{
+    public function resize($size="", $filename = "")
+    {
+        $this->autoRender = false;
+        $dimensions = explode('x', $size);
+        if(!isset($dimensions[0])){
+            $dimensions[0] = 150;
+        }
+        if(!isset($dimensions[1])){
+            $dimensions[1] = $dimensions[0];
+        }
+        $width = (int)$dimensions[0];
+        $height = (int)$dimensions[1];
+        if($width > 0 && $height > 0){
+            $cacheFilename = 'img' . DS . 'resize' . DS . $size . DS . $filename;
+            if(!file_exists($cacheFilename)){
+                $cacheFilename = Image::open('img' . DS . $filename)
+                ->zoomCrop($width, $height, 'transparent', 'center', 'center')
+                ->save($cacheFilename);
+            }
+            $this->redirect(DS.$cacheFilename);
+        }
+    }
+}
