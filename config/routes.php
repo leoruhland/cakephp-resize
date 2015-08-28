94793687945ab67e26ab00b28b7474218b889611
Leonardo Ruhland <leoruhland@gmail.com>
2015-08-28 07:44:26 -0300

    Removed unused route

--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,5 @@ use Cake\Routing\Router;
 
 Router::plugin('Resize', ['path' => '/'], function ($routes) {
     $routes->connect('/img/resize/:size/:filename', ['controller' => 'Resize', 'action' => 'resize'], ['pass' => ['size','filename']]);
-    $routes->connect('/img/crop/:size/:filename', ['controller' => 'Resize', 'action' => 'crop'], ['pass' => ['size','filename']]);
     $routes->fallbacks('InflectedRoute');
 });
