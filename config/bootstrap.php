88e77981ec6701296fc08d066240694f265b2698
Leonardo Ruhland <leoruhland@gmail.com>
2016-02-17 19:01:23 -0200

    Fix configuration array

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,7 +2,7 @@
 
 use Cake\Core\Configure;
 
-Configure::write('Resize.settings', [
+Configure::write('Resize', [
     'defaultSize' => [50, 50], //for invalid sizes
     'sizes' => [], //whitelist of sizes[[100,100], [200,200]],
     'maxSize' => [1920, 1920], //you can define a maxSize
--- src/Controller/ResizeController.php
+++ src/Controller/ResizeController.php
@@ -19,7 +19,7 @@ class ResizeController extends AppController
             $this->Auth->allow('resize');
         }
 
-        $this->config = Configure::read('Resize.settings');
+        $this->config = Configure::read('Resize');
     }
 
     public function resize($fullSize="", $filename = "")
